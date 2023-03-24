6d81c506acee508cca12f3eaccf59746aacc30cf
FarengarsAnvil <99881067+FarengarsAnvil@users.noreply.github.com>
2023-03-24 12:48:52 +0000

    Add files via upload

--- Force.java
+++ Force.java
@@ -18,22 +18,15 @@ private Boolean canFight;
      * @param fleetRef = String variable of a valid fleet reference
      * @param force = String variable representing the name of the Force
      * @param activationCost = Integer representing the activation Fee of the Force Object
-     * @param battleValue =  Integer representing the battle Strength of the Force Object
-     * @param cloakVal = Boolean Variable. If true: The Force Object has a Cloak. False: No Cloak.
-     * @param skirmish = Boolean Variable. If true: The force can partake in Skirmishes. False: Cannot take part in Skirmish.
-     * @param ambush =   Boolean Variable. If true: The force can partake in Ambushes. False: Cannot take part in Ambushes.
-     * @param fight=     Boolean Variable. If true: The force can partake in Fights. False: Cannot take part in Fights.
+
      */
-public  Force(String fleetRef, String force,  int activationCost, int battleValue, Boolean cloakVal, Boolean skirmish, Boolean ambush, Boolean fight) {
+public  Force(String fleetRef, String force,  int activationCost, int battleValue) {
     fleetReference = fleetRef;
     forceName = force;
     activationFee = activationCost;
     forceStatus = ForceState.DOCKED;
     battleStrength = battleValue;
-    hasCloak = cloakVal;
-    canSkirmish = skirmish;
-    canAmbush = ambush;
-    canFight = fight;
+
 }
 
 //TODO:: Accessor Method returns Fleet Reference.
@@ -48,7 +41,7 @@ public String getForceName() {
 public int getActivationFee() {
     return activationFee;
 }
-//TODO:: Accessor Method returns Battle Strenght of Force Object
+//TODO:: Accessor Method returns Battle Strength of Force Object
 public int getBattleStrength() {
     return battleStrength;
 }
